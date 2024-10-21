ada2c9bbb6646670db512f735c827becd167a6f2
heeju <bbbb412@naver.com>
2024-10-21 21:59:24 +0900

    241018

---
+++ 20241018/src/Main.java
@@ -0,0 +1,33 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        int index = sc.nextInt();
+        sc.close();
+        LinkedList<Integer> ll = (LinkedList)IntStream.rangeClosed(1, cnt).boxed().collect(Collectors.toCollection(LinkedList::new));
+        StringBuffer sb = new StringBuffer();
+        sb.append("<");
+        int i = 0;
+
+        while(ll.size() > 1) {
+            i = (i + index - 1) % ll.size();
+            sb.append(ll.remove(i)).append(", ");
+        }
+
+        sb.append(ll.remove()).append(">");
+        System.out.println(sb);
+    }
+}
